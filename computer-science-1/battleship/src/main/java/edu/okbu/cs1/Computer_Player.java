e97324bf2b46457d2fabfa6b205eee92de86985f
Caleb Payn <calebpayn23@gmail.com>
2025-05-01 10:18:54 -0500

    done

--- computer-science-1/battleship/src/main/java/edu/okbu/cs1/Computer_Player.java
+++ computer-science-1/battleship/src/main/java/edu/okbu/cs1/Computer_Player.java
@@ -4,6 +4,15 @@ import java.util.Random;
 public class Computer_Player {
     int ran_row;
     int ran_col;
+    boolean hit = false;
+    int leftSidePoint_row;
+    int leftSidePoint_col;
+    int topPoint_row;
+    int topPoint_col;
+    int rightSidePoint_row;
+    int rightSidePoint_col;
+    int bottemSide_row;
+    int bottemSide_col;
 
 
     public void random(int x, int y){
@@ -11,16 +20,35 @@ public class Computer_Player {
 
         ran_row = random.nextInt(x-1);
         ran_col = random.nextInt(y-1);
+
+        leftSidePoint_row = ran_row;
+        leftSidePoint_col = ran_col - 1;
+
+        topPoint_row = ran_row + 1;
+        topPoint_col = ran_col;
+
+
+        
         
     }
 
     public void com_player(int ran_row, int ran_col, String player[][], String hidden[][]){
-        if (player[ran_row][ran_col].equals("#")){
+        if (player[ran_row][ran_col].equals("#") && hit == false){
             if (hidden[ran_row][ran_col].equals("#")){
                 hidden[ran_row][ran_col] = "M";
                 player[ran_row][ran_col] = "M";
+                hit = false;
+            }
+            else {
+                hidden[ran_row][ran_col] = "H";
+                player[ran_row][ran_col] = "H";
+                hit = true;
             }
         }
-        else if ()
+        else if(hit == true){
+            if (player[ran_row][ran_col - 1].equals("#")){
+                
+            }
+        }
     }
 }
--- computer-science-1/tictactoe/src/main/java/edu/okbu/cs1/Compter_Player.java
+++ computer-science-1/tictactoe/src/main/java/edu/okbu/cs1/Compter_Player.java
@@ -4,6 +4,167 @@ public class Compter_Player {
 
     public void com_player(String game[][]){
 
+        if ((game[0][0].equals("O")) && (game[0][1].equals("O")) && (game[0][2].equals("#"))){
+            game[0][2] = "O";
+            //O|O|#
+            //#|#|#
+            //#|#|#
+        }
+        else if ((game[1][0].equals("O")) && (game[1][1].equals("O")) && (game[1][2].equals("#"))){
+            game[1][2] = "O";
+            //#|#|#
+            //O|O|#
+            //#|#|#
+        }
+        else if ((game[2][0].equals("O")) && (game[2][1].equals("O")) && (game[2][2].equals("#"))){
+            game[2][2] = "O";
+            //#|#|#
+            //#|#|#
+            //O|O|#
+        }
+
+
+        else if ((game[0][1].equals("O")) && (game[0][2].equals("O")) && (game[0][0].equals("#"))){
+            game[0][0] = "O";
+            //#|O|O
+            //#|#|#
+            //#|#|#
+        }
+        else if ((game[1][1].equals("O")) && (game[1][2].equals("O")) && (game[1][0].equals("#"))){
+            game[1][0] = "O";
+            //#|#|#
+            //#|O|O
+            //#|#|#
+        }
+        else if ((game[2][1].equals("O")) && (game[2][2].equals("O")) && (game[2][0].equals("#"))){
+            game[2][0] = "O";
+            //#|#|#
+            //#|#|#
+            //#|O|O
+        }
+
+
+        else if ((game[0][0].equals("O")) && (game[1][0].equals("O")) && (game[2][0].equals("#"))){
+            game[2][0] = "O";
+            //O|#|#
+            //O|#|#
+            //#|#|#
+        }
+        else if ((game[0][1].equals("O")) && (game[1][1].equals("O")) && (game[2][1].equals("#"))){
+            game[2][1] = "O";
+            //#|O|#
+            //#|O|#
+            //#|#|#
+        }
+        else if ((game[0][2].equals("O")) && (game[1][2].equals("O")) && (game[2][2].equals("#"))){
+            game[2][2] = "O";
+            //#|#|O
+            //#|#|O
+            //#|#|#
+        }
+
+
+        else if ((game[2][0].equals("O")) && (game[1][1].equals("O")) && (game[0][2].equals("#"))){
+            game[0][2] = "O";
+            //#|#|#
+            //#|O|#
+            //O|#|#
+        }
+        else if ((game[0][2].equals("O")) && (game[1][1].equals("O")) && (game[2][0].equals("#"))){
+            game[2][0] = "O";
+            //#|#|O
+            //#|O|#
+            //#|#|#
+        }
+        else if ((game[0][0].equals("O")) && (game[1][1].equals("O")) && (game[2][2].equals("#"))){
+            game[2][2] = "O";
+            //O|#|#
+            //#|O|#
+            //#|#|#
+        }
+        else if ((game[1][1].equals("O")) && (game[2][2].equals("O")) && (game[0][0].equals("#"))){
+            game[0][0] = "O";
+            //#|#|#
+            //#|O|#
+            //#|#|O
+        }
+
+        else if ((game[1][0].equals("O")) && (game[2][0].equals("O")) && (game[0][0].equals("#"))){
+            game[0][0] = "O";
+            //#|#|#
+            //O|#|#
+            //O|#|#
+        }
+        else if ((game[1][1].equals("O")) && (game[2][1].equals("O")) && (game[0][1].equals("#"))){
+            game[0][1] = "O";
+            //#|#|#
+            //#|O|#
+            //#|O|#
+        }
+        else if ((game[1][2].equals("O")) && (game[2][2].equals("O")) && (game[0][2].equals("#"))){
+            game[0][2] = "O";
+            //#|#|#
+            //#|#|O
+            //#|#|O
+        }
+
+
+        else if ((game[0][0].equals("O")) && (game[0][2].equals("O")) && (game[0][1].equals("#"))){
+            game[0][1] = "O";
+            //O|#|O
+            //#|#|#
+            //#|#|#
+        }
+        else if ((game[1][0].equals("O")) && (game[1][2].equals("O")) && (game[1][1].equals("#"))){
+            game[1][1] = "O";
+            //#|#|#
+            //O|#|O
+            //#|#|#
+        }
+        else if ((game[2][0].equals("O")) && (game[2][2].equals("O")) && (game[2][1].equals("#"))){
+            game[2][1] = "O";
+            //#|#|#
+            //#|#|#
+            //O|#|O
+        }
+
+
+        else if ((game[0][0].equals("O")) && (game[2][0].equals("O")) && (game[1][0].equals("#"))){
+            game[1][0] = "O";
+            //O|#|#
+            //#|#|#
+            //O|#|#
+        }
+        else if ((game[0][1].equals("O")) && (game[2][1].equals("O")) && (game[1][1].equals("#"))){
+            game[1][1] = "O";
+            //#|O|#
+            //#|#|#
+            //#|O|#
+        }
+        else if ((game[0][2].equals("O")) && (game[2][2].equals("O")) && (game[1][2].equals("#"))){
+            game[1][2] = "O";
+            //#|#|O
+            //#|#|#
+            //#|#|O
+        }
+
+
+        else if ((game[0][0].equals("O")) && (game[2][2].equals("O")) && (game[1][1].equals("#"))){
+            game[1][1] = "O";
+            //O|#|#
+            //#|#|#
+            //#|#|O
+        }
+        else if ((game[2][0].equals("O")) && (game[0][2].equals("O")) && (game[1][1].equals("#"))){
+            game[1][1] = "O";
+            //#|#|O
+            //#|#|#
+            //O|#|#
+        }
+
+
+
+
         if ((game[0][0].equals("X")) && (game[0][1].equals("X")) && (game[0][2].equals("#"))){
             game[0][2] = "O";
             //X|X|#
