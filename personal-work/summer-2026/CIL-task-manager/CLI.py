55cbabbdc2044c966f28ebadaee6f66ddf3595cb
Beb200 <calebpayn23@gmail.com>
2026-05-26 08:04:10 -0500

    add delete function

--- personal-work/summer-2026/CIL-task-manager/CLI.py
+++ personal-work/summer-2026/CIL-task-manager/CLI.py
@@ -14,7 +14,7 @@ def menu():
             print("[1] List")
             print("[2] Add")
             print("[3] ")
-            print("[4] ")
+            print("[4] delete")
             print("[0] Quit")
             print("--------------------------------------------")
 
@@ -31,6 +31,7 @@ def menu():
                     print("3")
                 case '4':
                     print("4")
+                    delete()
                 case '9':
                     print("test")
                 case '0':
@@ -76,6 +77,38 @@ def add():
                 continue
     except Exception as e:
         print(f"An unexpected error occurred in list: {e}")
+
+def delete():
+    try:
+        with open('info.json', 'r') as file:
+            data = json.load(file)
+        clear
+        while True:
+            print()
+            d_task = input("What is the task that you are deleting? ")
+
+            d_data = data.pop(d_task, None)
+
+            print()#/n
+            print(d_data)
+
+            if d_data == None:
+                print()#/n
+                print("No task found. Please try again.")
+                continue
+            elif input("Do you want to delete another task? Y/N ") == ("n" or "N"):
+                with open('info.json', 'w') as file:
+                   json.dump(data, file, indent=4)
+                print(data)
+                return
+            else:
+                continue
+
+
+    
+    except Exception as e:
+        print(f"An unexpected error occurred in list: {e}")
+    
 
 if __name__ == "__main__":
     menu()
